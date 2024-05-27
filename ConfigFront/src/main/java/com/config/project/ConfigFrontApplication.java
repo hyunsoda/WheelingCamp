0728551f6b2f9ac53f69e8f0b893859328c13361
LEECHUNGs <ragnacrimson@naver.com>
2024-05-27 10:29:03 +0900

    new project

---
+++ ConfigFront/src/main/java/com/config/project/ConfigFrontApplication.java
@@ -0,0 +1,13 @@
+package com.config.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigFrontApplication.class, args);
+	}
+
+}
