12cc43cf9fdc6f470a8d7ad395d6436cc1604718
LEECHUNGs <ragnacrimson@naver.com>
2024-05-29 17:05:23 +0900

    signUp

--- wheelingcamp/src/main/java/kr/co/wheelingcamp/member/model/service/MemberService.java
+++ wheelingcamp/src/main/java/kr/co/wheelingcamp/member/model/service/MemberService.java
@@ -1,5 +1,13 @@
 package kr.co.wheelingcamp.member.model.service;
 
+import kr.co.wheelingcamp.member.model.dto.Member;
+
 public interface MemberService {
+
+	/** 일반 회원가입
+	 * @param member : 가입 회원 정보
+	 * @return
+	 */
+	int signUp(Member member);
 
 }
