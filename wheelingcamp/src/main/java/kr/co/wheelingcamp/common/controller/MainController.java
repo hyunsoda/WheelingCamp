c9b13205e0b32e55c7137f1785d3bb8860f3b73e
JUHAHAH <108599647+JUHAHAH@users.noreply.github.com>
2024-05-29 16:40:16 +0900

    파일(이미지 파일) 업로드 기능

--- wheelingcamp/src/main/java/kr/co/wheelingcamp/common/controller/MainController.java
+++ wheelingcamp/src/main/java/kr/co/wheelingcamp/common/controller/MainController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
-	
+
 	@RequestMapping("/")
 	public String home() {
-		return "pages/home";
+		return "pages/test";
 	}
-	
+
 }
