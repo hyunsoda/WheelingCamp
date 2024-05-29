727458a7f82801eda44b298055345800998e8ba7
hyunsoda <so0144@naver.com>
2024-05-29 16:57:51 +0900

    fix

--- wheelingcamp/src/main/java/kr/co/wheelingcamp/member/controller/MemberController.java
+++ wheelingcamp/src/main/java/kr/co/wheelingcamp/member/controller/MemberController.java
@@ -1,19 +1,38 @@
 package kr.co.wheelingcamp.member.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import jakarta.servlet.http.HttpServletRequest;
+import kr.co.wheelingcamp.member.model.dto.Member;
 import kr.co.wheelingcamp.member.model.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@SessionAttributes({"loginMember"})
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("member")
 public class MemberController {
 
 	private final MemberService service;
+	
+	
 	
 	/** 로그인 페이지 redirect
 	 * @return
@@ -37,5 +56,122 @@ public class MemberController {
 		
 		return "";
 	}
+	
+	
+	/** 네이버 로그인 페이지 redirect
+	 * @return naverLoginUrl
+	 */
+	@GetMapping("loginNaver")
+	public String loginNaverView(HttpServletRequest request) {
+		
+		String naverLoginUrl = service.naverLoginUrl(request);
+		
+		return "redirect:" + naverLoginUrl;
+	}
+	
+	
+	/** 네이버 로그인 callBack
+	 * @return
+	 */
+	@GetMapping("naverCallBack")
+	public String naverCallBack(HttpServletRequest request, RedirectAttributes ra, Model model) {
+		
+		// 네이버에서 전달받은 code, state값 가져오기
+		String code = request.getParameter("code");
+		String state = request.getParameter("state");
+		
+		// 세션에 저장해둔 state값 가져오기
+		String sessionState = request.getSession().getAttribute("state").toString();
+		
+		
+		// CSRF 공격 방지를 위해 state 값 비교하기
+		if(!state.equals(sessionState)) {
+			request.getSession().removeAttribute("state");
+			return "redirect:/";
+		}
+		
+		
+		// 기본 tokenURL 주소 
+		String tokenUrl = "https://nid.naver.com/oauth2.0/token";
+		
+		// body data 생성
+		MultiValueMap<String, String> parameter = service.naverTockenUrl(code, state);
+		
+		
+		// request header 설정하기
+		HttpHeaders headers = new HttpHeaders();
+		
+		// Content-type 설정
+		headers.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+		
+		// header와 body로 Request 생성
+		HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<>(parameter,headers);
+		
+		
+		try {
+			
+			RestTemplate restTemplate = new RestTemplate();
+			
+			// 응답 데이터(json)을 Map으로 받을 수 있도록 관련 메시지 컨버터 추가하기
+			restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
+			
+			// Post 방식으로 Http 요청하기
+			// 응답 데이터 형식은 Hashmap
+			ResponseEntity<HashMap> result = restTemplate.postForEntity(tokenUrl, entity, HashMap.class);
+			
+			// result의 body 꺼내오기
+			Map<String, String> resMap = result.getBody();
+			
+			// accessToken 가져오기
+			String accessToken = resMap.get("access_token");
+			
+			
+			// userinfo 가져오는 base url
+			String userInfoUrl = "https://openapi.naver.com/v1/nid/me";
+			
+			// Header에 accessToken 삽입
+			headers.set("Authorization", "Bearer "+accessToken);
+			
+			// Request entity 생성하기
+			HttpEntity<?> userInfoEntity = new HttpEntity<>(headers);
+			
+			
+			// Post 방식으로 Http 요청
+			// 응답 데이터 형식은 Hashmap
+			ResponseEntity<HashMap> userResult = restTemplate.postForEntity(userInfoUrl, userInfoEntity,HashMap.class);
+			Map<String, Map<String, String>> userResultMap = userResult.getBody();
+			
+			
+			// Member 객체에 값 세팅해서 회원가입 or 로그인 수행하기
+			Member naverMember = service.naverLogin(userResultMap.get("response"));
+			
+			// 로그인 실패인 경우
+			if(naverMember == null) {
+				
+				String message = "회원가입 실패";
+				ra.addFlashAttribute("message", message);
+				
+				return "member/login";
+				
+			} 
+			
+			// session scope에 loginMember 넣기
+			model.addAttribute("loginMember", naverMember);
+			
+			// 세션에 저장된 state값 삭제
+			request.getSession().removeAttribute("state");
+			
+			
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return "member/loginComplete";
+	}
+	
+
+	
+	
 	
 }
--- wheelingcamp/src/main/java/kr/co/wheelingcamp/member/model/service/MemberServiceImpl.java
+++ wheelingcamp/src/main/java/kr/co/wheelingcamp/member/model/service/MemberServiceImpl.java
@@ -1,14 +1,134 @@
 package kr.co.wheelingcamp.member.model.service;
 
-import org.springframework.stereotype.Service;
+import java.math.BigInteger;
+import java.security.SecureRandom;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import jakarta.servlet.http.HttpServletRequest;
+import kr.co.wheelingcamp.member.model.dto.Member;
 import kr.co.wheelingcamp.member.model.mapper.MemberMapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class MemberServiceImpl implements MemberService{
 
 	private final MemberMapper mapper;
+
+	// 네이버 클라이언트 ID
+    @Value("${naver.client_id}")
+    private String naverClientId;
+    
+    // 네이버 클라이언트 redirect_uri
+    @Value("${naver.redirect_uri}")
+    private String naverRedirectUri;
+    
+    // 네이버 클라리언트 secret
+    @Value("${naver.client_secret}")
+    private String naverClientSecret;
+    
+ 
+
+    
+	// 네이버 로그인 인가코드 요청
+	@Override
+	public String naverLoginUrl(HttpServletRequest request) {
+		
+		StringBuilder sb = new StringBuilder();
+		
+		// state값 설정
+		String state = generateState();
+		
+		// naverLoginUrl  공통 부분
+		String baseUrl = "https://nid.naver.com/oauth2.0/authorize?response_type=code";
+		
+		// naverLoginUrl 조립
+		sb.append(baseUrl);
+	    sb.append("?response_type=code");
+	    sb.append("&client_id=" + naverClientId);
+	    sb.append("&redirect_uri=" + naverRedirectUri);
+	    sb.append("&state=" + state);
+		
+		
+	    // 세션에 state 저장
+	    request.getSession().setAttribute("state", state);
+	    
+		log.debug("state 값 확인" + state);
+		return sb.toString();
+	}
+	
+	// 네이버 state 랜덤값 얻어오기 
+    public String generateState(){
+        SecureRandom random = new SecureRandom();
+        return new BigInteger(130, random).toString(32);
+    }
+	
+    // 네이버 tocken 얻어오기 위한 body data 생성
+	@Override
+	public MultiValueMap<String, String> naverTockenUrl(String code, String state) {
+		
+		// body data 생성하기
+		MultiValueMap<String, String> parameter = new LinkedMultiValueMap<>();
+		parameter.add("grant_type", "authorization_code");
+	    parameter.add("client_id", naverClientId);
+	    parameter.add("client_secret", naverClientSecret);
+	    parameter.add("code", code);
+	    parameter.add("state", state);
+		
+		return parameter;
+	}
+	
+	// 네이버 로그인 수행하기 
+	@Override
+	public Member naverLogin(Map<String, String> map) {
+		
+		// 아이디 중복 검사하기 
+		String checkId = "naver" + map.get("id");
+		int result = mapper.checkId(checkId);
+		
+		// map에 "naver"를 붙인 새로운 아이디 넣어주기
+		map.put("checkId", checkId);
+		
+		// 전화번호 형식 수정 "- 부분 없애기"
+		String PhoneNumber = (map.get("mobile").toString()).replaceAll("-", "");
+        map.put("fixPhoneNumber", PhoneNumber);
+		
+        // 생년월일 형식 수정 "-부분 없애고 년도, 월일 이어붙이기"
+        String monthDay = map.get("birthday").replaceAll("-", "");
+        String fixBirthday = (map.get("birthyear") + monthDay);
+        map.put("fixBirthday", fixBirthday);
+        
+        Member loginMember = null;
+		
+		// 아이디가 존재할 경우 로그인 진행
+		if(result > 0) {
+			
+			loginMember = mapper.naverLoginMember(map);
+			return loginMember;
+			
+		} else { // 아이디가 없는 경우 회원가입 진행
+			
+			int signUpResult = mapper.naverSignUp(map);
+			
+			
+			if(signUpResult == 0) { // 회원가입 실패
+				
+				return null;
+				
+			} else { // 회원가입 성공인 경우 로그인진행
+				
+				loginMember = mapper.naverLoginMember(map);
+				return loginMember;
+			}
+			
+		}
+	}
 	
 }
