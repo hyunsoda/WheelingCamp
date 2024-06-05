4eadf0317efc04c4bf6f01ae1b4c559d7bc57eef
LEECHUNGs <ragnacrimson@naver.com>
2024-06-05 17:28:57 +0900

    check

--- wheelingcamp/src/main/java/kr/co/wheelingcamp/item/model/service/ItemService.java
+++ wheelingcamp/src/main/java/kr/co/wheelingcamp/item/model/service/ItemService.java
@@ -1,5 +1,6 @@
 package kr.co.wheelingcamp.item.model.service;
 
+import java.util.List;
 import java.util.Map;
 
 import kr.co.wheelingcamp.item.model.dto.Item;
@@ -27,4 +28,18 @@ public interface ItemService {
 	 * @return
 	 */
 	Map<String, Object> selectCategoryAll(Map<String, Object> map);
+
+	/**
+	 * 차급 목록 가져오기
+	 * 
+	 * @return
+	 */
+	List<String> selectCarGrade();
+
+	/**
+	 * 캠핑용품 카테고리 가져오기
+	 * 
+	 * @return
+	 */
+	List<String> selectEquipmentCategory();
 }
