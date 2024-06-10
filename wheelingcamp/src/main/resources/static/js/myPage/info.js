a4208da5e52c74218254fad217832e724d1a7380
minnily <dlalsrud20142905@gmail.com>
2024-06-10 10:37:57 +0900

    6.10
    ㅇㅇㅇㅇ

--- wheelingcamp/src/main/resources/static/js/myPage/info.js
+++ wheelingcamp/src/main/resources/static/js/myPage/info.js
@@ -200,60 +200,60 @@ secessionBtn.addEventListener("click", (e) => {
       } else {
         // 일반인 경우
         profileModal.setAttribute("aria-hidden", "false");
-
-        const checkSecession = {
-          agree: false,
-          currentPw: false,
-        };
-
-        // 비밀번호 입력 시 이벤트
-        currentPw.addEventListener("input", () => {
-          if (currentPw.value.trim().length == 0) {
-            pwMessage.innerText = "비밀번호를 입력해주세요";
-            checkSecession.currentPw = false;
-            return;
-          }
-          fetch("/myPage/checkPw", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: currentPw.value,
-          })
-            .then((resp) => resp.json())
-            .then((result) => {
-              if (result == 0) {
-                pwMessage.innerText = "비밀번호 불일치";
-                checkSecession.currentPw = false;
-                return;
-              }
-              pwMessage.innerText = "비밀번호 일치";
-              checkSecession.currentPw = true;
-            });
-        });
-
-        // 동의체크박스
-        agree.addEventListener("change", (e) => {
-          console.log(e.target.checked);
-          if (e.target.checked) checkSecession.agree = true;
-          else checkSecession.agree = false;
-        });
-
-        // 최종 서브밋 될 때 이벤트
-        secessionForm.addEventListener("submit", (e) => {
-          if (!checkSecession.agree) {
-            e.preventDefault();
-            alert("탈퇴 약관에 동의해주세요");
-            return;
-          }
-
-          if (!checkSecession.currentPw) {
-            e.preventDefault();
-            alert("비밀번호가 일치하지 않습니다");
-            return;
-          }
-
-          alert("탈퇴 되었습니다!");
-          return true;
-        });
       }
     });
+});
+
+const checkSecession = {
+  agree: false,
+  currentPw: false,
+};
+
+// 비밀번호 입력 시 이벤트
+currentPw.addEventListener("input", () => {
+  if (currentPw.value.trim().length == 0) {
+    pwMessage.innerText = "비밀번호를 입력해주세요";
+    checkSecession.currentPw = false;
+    return;
+  }
+  fetch("/myPage/checkPw", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: currentPw.value,
+  })
+    .then((resp) => resp.json())
+    .then((result) => {
+      if (result == 0) {
+        pwMessage.innerText = "비밀번호 불일치";
+        checkSecession.currentPw = false;
+        return;
+      }
+      pwMessage.innerText = "비밀번호 일치";
+      checkSecession.currentPw = true;
+    });
+});
+
+// 동의체크박스
+agree.addEventListener("change", (e) => {
+  console.log(e.target.checked);
+  if (e.target.checked) checkSecession.agree = true;
+  else checkSecession.agree = false;
+});
+
+// 최종 서브밋 될 때 이벤트
+secessionForm.addEventListener("submit", (e) => {
+  if (!checkSecession.agree) {
+    e.preventDefault();
+    alert("탈퇴 약관에 동의해주세요");
+    return;
+  }
+
+  if (!checkSecession.currentPw) {
+    e.preventDefault();
+    alert("비밀번호가 일치하지 않습니다");
+    return;
+  }
+
+  alert("탈퇴 되었습니다!");
+  return true;
 });
