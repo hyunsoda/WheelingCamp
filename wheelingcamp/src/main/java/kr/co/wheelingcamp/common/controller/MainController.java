f4726c28cb69f9e6a3a80d8cfb2fe9c9ec97e607
JUHAHAH <108599647+JUHAHAH@users.noreply.github.com>
2024-06-14 09:02:06 +0900

    아침머지

--- wheelingcamp/src/main/java/kr/co/wheelingcamp/common/controller/MainController.java
+++ wheelingcamp/src/main/java/kr/co/wheelingcamp/common/controller/MainController.java
@@ -1,6 +1,7 @@
 package kr.co.wheelingcamp.common.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -8,7 +9,12 @@ public class MainController {
 
 	@RequestMapping("/")
 	public String home() {
-		return "aiRecomm";
+		return "pages/home";
+	}
+
+	@GetMapping("/aiRecomm")
+	public String aiRecomm() {
+		return "pages/aiRecomm";
 	}
 
 }
