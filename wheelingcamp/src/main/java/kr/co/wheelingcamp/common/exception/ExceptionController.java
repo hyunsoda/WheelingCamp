283607b674e7397e9e1bb589f28c7082cbcde85c
LEECHUNGs <ragnacrimson@naver.com>
2024-06-21 17:31:16 +0900

    리엑트는 악이다

--- wheelingcamp/src/main/java/kr/co/wheelingcamp/common/exception/ExceptionController.java
+++ wheelingcamp/src/main/java/kr/co/wheelingcamp/common/exception/ExceptionController.java
@@ -1,54 +1,45 @@
 package kr.co.wheelingcamp.common.exception;
 
-import java.io.PrintWriter;
-import java.io.StringWriter;
-
-import org.apache.coyote.BadRequestException;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.server.MethodNotAllowedException;
 
 import lombok.extern.slf4j.Slf4j;
 
 @ControllerAdvice
 @Slf4j
 public class ExceptionController {
-		
 
-	   @ExceptionHandler(BadRequestException.class)
-	    public String handleBadRequestException(BadRequestException e, Model model) {
-          StringWriter sw = new StringWriter();
-	        PrintWriter pw = new PrintWriter(sw);
-	        e.printStackTrace(pw);
-	        String stackTrace = sw.toString();
-
-	        model.addAttribute("e", e);
-	        return "error/404"; // 404 에러 페이지 경로
-	    }
-
-	    @ExceptionHandler(MethodNotAllowedException.class)
-	    public String handleMethodNotAllowedException(MethodNotAllowedException e, Model model) {
-	       StringWriter sw = new StringWriter();
-	        PrintWriter pw = new PrintWriter(sw);
-	        e.printStackTrace(pw);
-	        String stackTrace = sw.toString();
-
-	        model.addAttribute("e", e);
-	        return "error/405"; // 405 에러 페이지 경로
-	    }
-
-	    
-	    @ExceptionHandler(Exception.class)
-	    public String handleError(Exception e, Model model) {
-	        StringWriter sw = new StringWriter();
-	        PrintWriter pw = new PrintWriter(sw);
-	        e.printStackTrace(pw);
-	        String stackTrace = sw.toString();
-
-	        model.addAttribute("e", e);
-	        return "error/500";
-	    }
+//	@ExceptionHandler(BadRequestException.class)
+//	public String handleBadRequestException(BadRequestException e, Model model) {
+//		StringWriter sw = new StringWriter();
+//		PrintWriter pw = new PrintWriter(sw);
+//		e.printStackTrace(pw);
+//		String stackTrace = sw.toString();
+//
+//		model.addAttribute("e", e);
+//		return "error/404"; // 404 에러 페이지 경로
+//	}
+//
+//	@ExceptionHandler(MethodNotAllowedException.class)
+//	public String handleMethodNotAllowedException(MethodNotAllowedException e, Model model) {
+//		StringWriter sw = new StringWriter();
+//		PrintWriter pw = new PrintWriter(sw);
+//		e.printStackTrace(pw);
+//		String stackTrace = sw.toString();
+//
+//		model.addAttribute("e", e);
+//		return "error/405"; // 405 에러 페이지 경로
+//	}
+//
+//	@ExceptionHandler(Exception.class)
+//	public String handleError(Exception e, Model model) {
+//		StringWriter sw = new StringWriter();
+//		PrintWriter pw = new PrintWriter(sw);
+//		e.printStackTrace(pw);
+//		String stackTrace = sw.toString();
+//
+//		model.addAttribute("e", e);
+//		return "error/500";
+//	}
 
 //	 @ExceptionHandler(BadRequestException.class)
 //	    public String handleBadRequestException(BadRequestException e, Model model) {
@@ -76,5 +67,4 @@ public class ExceptionController {
 //        return "error/sum"; // 혹은 적절한 에러 페이지 경로
 //    }
 
-	
 }
