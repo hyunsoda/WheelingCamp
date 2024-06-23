7ec822dbdc7dc3008ce9b0a135bc639adc1a75e7
minnily <156267800+minnily@users.noreply.github.com>
2024-06-24 06:30:52 +0900

    6.23
    뱃지부분 마무리

---
+++ wheelingcamp/src/main/java/kr/co/wheelingcamp/common/util/ImageParsingRequest.java
@@ -0,0 +1,10 @@
+package kr.co.wheelingcamp.common.util;
+
+public class ImageParsingRequest {
+
+	public Object url() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- wheelingcamp/src/main/java/kr/co/wheelingcamp/common/util/NaverOcrApi.java
+++ wheelingcamp/src/main/java/kr/co/wheelingcamp/common/util/NaverOcrApi.java
@@ -2,231 +2,155 @@ package kr.co.wheelingcamp.common.util;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.net.HttpURLConnection;
+import java.net.ProtocolException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.nio.charset.StandardCharsets;
+import java.util.LinkedHashMap;
 import java.util.UUID;
 
 import org.apache.tomcat.util.json.JSONParser;
+import org.apache.tomcat.util.json.ParseException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.configurationprocessor.json.JSONArray;
+import org.springframework.boot.configurationprocessor.json.JSONException;
 import org.springframework.boot.configurationprocessor.json.JSONObject;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StopWatch;
 
-import jakarta.mail.internet.ParseException;
+import groovyjarjarantlr4.v4.parse.ANTLRParser.exceptionGroup_return;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@Component
+@Service
 public class NaverOcrApi {
 
 	@Value("${naver.service.url}")
-	private String url;
+	private static String url;
 	
-//		@GetMapping("/licenseTest2")
-//		@ResponseBody
-//		public String checkLicense(String code) {
-//
-//			RestTemplate rt = new RestTemplate();
-//
-//			// 요청 헤더 설정
-//			HttpHeaders headers = new HttpHeaders();
-//			headers.add("Content-type", "application/json");
-//			headers.add("X-OCR-SECRET", "eEh3bWJCY3hMdnhjWVd1TFB0VlRvaVhYTUlhSklTUms=");
-//
-//			// 요청 본문 설정
-//			MultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
-//			  
-//			Map<String, Object> image = new HashMap<>();
-//	        image.put("format", "png");
-//	        image.put("name", "medium");
-//	        image.put("data", null);
-//	        image.put("url", "https://image.ajunews.com/content/image/2023/06/07/20230607083035644114.jpg");
-//	        
-//	        params.add("images", Collections.singletonList(image));
-//			params.add(" lang", "ko");
-//			params.add("requestId", "string");
-//			params.add("resultType", "string");
-//			params.add("timestamp","{{$timestamp}}");
-//			params.add("version", "V1");
-//
-//			// 요청 헤더 + 본문
-//			HttpEntity<MultiValueMap<String, Object>> checkLicense = new HttpEntity<>(params, headers);
-//
-//			// 응답 본문 받기
-//			ResponseEntity<Map<String, Object>> response = rt.exchange("https://4wekbqwdra.apigw.ntruss.com/custom/v1/32001/d60df6998b195d6ffc6633b8c21e2f14aa40541587e24702a19db4f4fbde87de/general?=",
-//					HttpMethod.POST, checkLicense, new ParameterizedTypeReference<Map<String, Object>>() {
-//					});
-//
-//			// 받은 응답 본문을 map으로 변환
-//			Map<String, Object> responseBody = response.getBody();
-//			log.info("responseBody:{}",responseBody.toString());
-//			// 토큰 리턴
-//			return "";
-//		}
-	/**
-     * 네이버 ocr api 호출한다
-     * @param {string} type 호출 메서드 타입
-     * @param {string} filePath 파일 경로
-     * @param {string} naver_secretKey 네이버 시크릿키 값
-     * @param {string} ext 확장자
-     * @returns {List} 추출 text list
-     */
-    @SuppressWarnings("unchecked")
-	public  List<String> callApi(String type, String filePath, String naver_secretKey, String ext) {
-        String apiURL = url;
-        String secretKey = naver_secretKey;
-        String imageFile = filePath;
-        List<String> parseData = null;
+	@Value("{naver.service.secretKey}")
+	private static String secretKey;
+	
+	public static String execute(ImageParsingRequest request) throws Exception{
+        
+            StopWatch totalTime = new StopWatch();
+            totalTime.start();
 
-        log.info("callApi Start!");
+            // ----------- 요청 전송 ---------------------
+            StopWatch requestStopWatch = new StopWatch();
+            requestStopWatch.start();
 
-        try {
-            URL url = new URL(apiURL);
-            HttpURLConnection con = (HttpURLConnection)url.openConnection();
-            con.setUseCaches(false);
-            con.setDoInput(true);
-            con.setDoOutput(true);
-            con.setReadTimeout(30000);
-            con.setRequestMethod(type);
-            String boundary = UUID.randomUUID().toString().replaceAll("-", "");
-            con.setRequestProperty("Content-Type", "application/json");
-            con.setRequestProperty("X-OCR-SECRET", secretKey);
+            URL newUrl = new URL(url);
+            HttpURLConnection connection = createRequestHeader(newUrl);
+            createRequestBody(connection, request);
 
-            JSONObject json = new JSONObject();
-            json.put("version", "V1");
-            json.put("requestId", "string");
-            json.put("timestamp", "{{$timestamp}}");
-            JSONObject image = new JSONObject();
-            image.put("format", ext);
-            image.put("name", "demo");
-            JSONArray images = new JSONArray();
-            ((List<String>) images).addAll((Collection<? extends String>) image);
-            json.put("images", images);
-            String postParams = json.toString();
+            requestStopWatch.stop();
+            System.out.println("request 생성 시간 : " + requestStopWatch.getTotalTimeMillis() + "ms");
 
-            con.connect();
-            DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-            File file = new File(imageFile);
-            writeMultiPart(wr, postParams, file, boundary);
-            wr.close();
+            // ----------- 응답 수신 ---------------------
+            StopWatch responseStopWatch = new StopWatch();
+            responseStopWatch.start();
 
-            int responseCode = con.getResponseCode();
-            BufferedReader br;
-            if (responseCode == 200) {
-                br = new BufferedReader(new InputStreamReader(con.getInputStream()));
-            } else {
-                br = new BufferedReader(new InputStreamReader(con.getErrorStream()));
-            }
-            String inputLine;
-            StringBuffer response = new StringBuffer();
-            while ((inputLine = br.readLine()) != null) {
-                response.append(inputLine);
-            }
-            br.close();
+            StringBuilder response = getResponseData(connection);
 
-            parseData = jsonparse(response);
+            responseStopWatch.stop();
+            System.out.println("응답 수신 시간 : " + responseStopWatch.getTotalTimeMillis() + "ms");
 
+            // ----------- 데이터 파싱 ---------------------
+            StopWatch parsingStopWatch = new StopWatch();
+            parsingStopWatch.start();
 
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-        return parseData;
+            StringBuilder result = parseResponseData(response);
+
+            parsingStopWatch.stop();
+            System.out.println("data parsing 시간 : " + parsingStopWatch.getTotalTimeMillis() + "ms");
+
+            totalTime.stop();
+            System.out.println("Total Time = " + totalTime.getTotalTimeMillis() + "ms");
+            return result.toString();
+
     }
 
-    /**
-     * writeMultiPart
-     * @param {OutputStream} out 데이터를 출력
-     * @param {string} jsonMessage 요청 params
-     * @param {File} file 요청 파일
-     * @param {String} boundary 경계
-     */
-    private static void writeMultiPart(OutputStream out, String jsonMessage, File file, String boundary) throws
-            IOException {
-        StringBuilder sb = new StringBuilder();
-        sb.append(boundary).append("\r\n");
-        sb.append("Content-Disposition:form-data; name=\"message\"\r\n\r\n");
-        sb.append(jsonMessage);
-        sb.append("\r\n");
-
-        out.write(sb.toString().getBytes("UTF-8"));
-        out.flush();
-
-        if (file != null && file.isFile()) {
-            out.write(("--" + boundary + "\r\n").getBytes("UTF-8"));
-            StringBuilder fileString = new StringBuilder();
-            fileString
-                    .append("Content-Disposition:form-data; name=\"file\"; filename=");
-            fileString.append("\"" + file.getName() + "\"\r\n");
-            fileString.append("Content-Type: application/octet-stream\r\n\r\n");
-            out.write(fileString.toString().getBytes("UTF-8"));
-            out.flush();
-
-            try (FileInputStream fis = new FileInputStream(file)) {
-                byte[] buffer = new byte[8192];
-                int count;
-                while ((count = fis.read(buffer)) != -1) {
-                    out.write(buffer, 0, count);
-                }
-                out.write("\r\n".getBytes());
-            }
-
-            out.write((boundary + "--\r\n").getBytes("UTF-8"));
-        }
-        out.flush();
+    private static HttpURLConnection createRequestHeader(URL newUrl) throws Exception {
+        HttpURLConnection connection = (HttpURLConnection) newUrl.openConnection();
+        connection.setUseCaches(false);
+        connection.setDoInput(true);
+        connection.setDoOutput(true);
+        connection.setReadTimeout(5000);
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "application/json;");
+        connection.setRequestProperty("X-OCR-SECRET", secretKey);
+        return connection;
     }
-    /**
-     * 데이터 가공
-     * @param {StringBuffer} response 응답값
-     * @returns {List} result text list
-     */
-    private static List<String> jsonparse(StringBuffer response) throws ParseException {
-        //json 파싱
-//        JSONParser jp = new JSON;
-//        JSONObject jobj = (JSONObject) jp.parse(response.toString());
-//        //images 배열 obj 화
-//        JSONArray JSONArrayPerson = (JSONArray)jobj.get("images");
-//        JSONObject JSONObjImage = (JSONObject)JSONArrayPerson.get(0);
-//        JSONArray s = (JSONArray) JSONObjImage.get("fields");
-//        //
-//        List<Map<String, Object>> m = JsonUtill.getListMapFromJsonArray(s);
-        List<String> result = new ArrayList<>();
-//        for (Map<String, Object> as : m) {
-//            result.add((String) as.get("inferText"));
-//        }
 
+    private static void createRequestBody(HttpURLConnection connection, ImageParsingRequest request) throws Exception {
+        JSONObject image = new JSONObject();
+        image.put("format", "PNG");
+        image.put("name", "requestImage");
+        image.put("url", request.url());
+
+        JSONArray images = new JSONArray();
+        images.put(image);
+
+        JSONObject requestObject = new JSONObject();
+        requestObject.put("version", "V2");
+        requestObject.put("requestId", UUID.randomUUID().toString());
+        requestObject.put("timestamp", System.currentTimeMillis());
+        requestObject.put("lang", "ko");
+        requestObject.put("resultType", "string");
+        requestObject.put("images", images);
+
+        connection.connect();
+        DataOutputStream outputStream = new DataOutputStream(connection.getOutputStream());
+        outputStream.write(requestObject.toString().getBytes(StandardCharsets.UTF_8));
+        outputStream.flush();
+        outputStream.close();
+    }
+
+    private static BufferedReader checkResponse(HttpURLConnection connection) throws Exception{
+        int responseCode = connection.getResponseCode();
+        BufferedReader reader;
+
+        if (responseCode == 200) {
+            reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        }
+        else {
+            reader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
+        }
+        return reader;
+    }
+
+    private static StringBuilder getResponseData(HttpURLConnection connection) throws Exception{
+        BufferedReader reader = checkResponse(connection);
+        String line;
+        StringBuilder response = new StringBuilder();
+        while ((line = reader.readLine()) != null) {
+            response.append(line);
+        }
+        reader.close();
+        return response;
+    }
+
+    private static StringBuilder parseResponseData(StringBuilder response) throws Exception {
+        JSONParser responseParser = new JSONParser(response.toString());
+        LinkedHashMap<String, String> hashMap = (LinkedHashMap<String, String>) responseParser.parse();
+        JSONObject parsed = new JSONObject(hashMap);
+        JSONArray parsedImages = (JSONArray) parsed.get("images");
+        StringBuilder result = new StringBuilder();
+
+        if (parsedImages != null) {
+            JSONObject parsedImage = (JSONObject) parsedImages.get(0);
+            JSONArray parsedTexts = (JSONArray) parsedImage.get("fields");
+
+            for (int i = 0; i < parsedTexts.length(); i++) {
+                JSONObject current = (JSONObject) parsedTexts.get(i);
+                result.append((String) current.get("inferText")).append(" ");
+            }
+        }
         return result;
-//    }
-}
-
-	
-	
-	
-	
-	
-	
+    }
 	
 	
 }
