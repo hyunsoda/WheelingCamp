1de3b9f47be2700e2b95bb7b0509eff83caed32f
minnily <dlalsrud20142905@gmail.com>
2024-06-24 11:01:55 +0900

    1

--- wheelingcamp/src/main/resources/static/js/util/licenseTest.js
+++ wheelingcamp/src/main/resources/static/js/util/licenseTest.js
@@ -1,3 +1,48 @@
-const file = document.querySelector("#file");
-const btn = document.querySelector("#btn");
+// const file = document.querySelector("#file");
+// const btn = document.querySelector("#btn");
+// const formImg = document.querySelector("#formImg");
 
+// formImg.addEventListener("submit", () => {});
+// function handleSubmit(e) {
+//   console.log("테스트");
+//   e.preventDefault();
+// }
+
+// file.addEventListener("change", (e) => {
+//   const fileItem = e.target.files[0];
+//   console.log(fileItem);
+//   if (fileItem) {
+//     uploadFile(fileItem);
+//   }
+// });
+
+// async function uploadFile(fileItem) {
+//   try {
+//     const formData = new FormData();
+//     formData.append("image", fileItem);
+
+//     const response = await fetch("/validateLicense/uploadImage", {
+//       method: "POST",
+//       body: formData,
+//     });
+
+//     if (response.ok) {
+//       const data = await response.json();
+//       // 서버에서 반환한 데이터 처리 로직 추가
+//       console.log(data);
+//     } else {
+//       console.error("파일 업로드에 실패했습니다.");
+//     }
+//   } catch (error) {
+//     console.error("오류 발생:", error);
+//   }
+// }
+
+// btn.addEventListener("click", () => {
+//   const fileItem = file.files[0];
+//   if (fileItem) {
+//     uploadFile(fileItem);
+//   } else {
+//     alert("파일을 선택하세요");
+//   }
+// });
