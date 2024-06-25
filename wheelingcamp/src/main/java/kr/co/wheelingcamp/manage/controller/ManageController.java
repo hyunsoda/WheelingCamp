51d089c07ffe2097ff925badb521e8278b4c4976
JUHAHAH <108599647+JUHAHAH@users.noreply.github.com>
2024-06-25 10:28:15 +0900

    asd

--- wheelingcamp/src/main/java/kr/co/wheelingcamp/manage/controller/ManageController.java
+++ wheelingcamp/src/main/java/kr/co/wheelingcamp/manage/controller/ManageController.java
@@ -28,7 +28,7 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("manage")
-@CrossOrigin(origins = "https://wheelingcamp-manager.vercel.app/")
+@CrossOrigin(origins = "https://wheelingcamp-manager.vercel.app")
 public class ManageController {
 
 	private final ManageService service;
@@ -110,7 +110,9 @@ public class ManageController {
 		return service.updateOrder(pay, payCode);
 	}
 
-	/** 주문 디테일 조회
+	/**
+	 * 주문 디테일 조회
+	 * 
 	 * @param payCode
 	 * @param payNo
 	 * @return
@@ -122,8 +124,10 @@ public class ManageController {
 		log.info("payNo : " + payNo);
 		return service.selectOneOrder(payCode, payNo);
 	}
-	
-	/** 주문 디테일 수정
+
+	/**
+	 * 주문 디테일 수정
+	 * 
 	 * @param payDetail
 	 * @return
 	 */
@@ -132,27 +136,31 @@ public class ManageController {
 		return service.updateOrderDetail(payDetail);
 	}
 
-	/** 신규 가입자 수 조회
+	/**
+	 * 신규 가입자 수 조회
+	 * 
 	 * @return
 	 */
 	@GetMapping("memberCount")
-	public List<Member> memberCount(){
+	public List<Member> memberCount() {
 		return service.memberCount();
 	}
 
-	/** 상품 일자별 뷰카운트 조회
+	/**
+	 * 상품 일자별 뷰카운트 조회
+	 * 
 	 * @return
 	 */
 	@GetMapping("itemViewCount")
-	public List<Item> itemViewCount(@RequestParam("categoryCode") int categoryCode){
+	public List<Item> itemViewCount(@RequestParam("categoryCode") int categoryCode) {
 		return service.itemViewCount(categoryCode);
 	}
-	
+
 	@GetMapping("logout")
 	public String logout(SessionStatus status) {
-		
+
 		status.setComplete();
-		
+
 		return "redirect:/";
 	}
 //--------------------------------------------------------------------------------------------------
@@ -220,10 +228,14 @@ public class ManageController {
 						return result;
 					}
 				} else {
-					//result = fileService.deleteImageAll(Integer.parseInt(item.get("itemNo").toString()), "item");
+					// result =
+					// fileService.deleteImageAll(Integer.parseInt(item.get("itemNo").toString()),
+					// "item");
 				}
 			} else {
-				//result = fileService.deleteImageAll(Integer.parseInt(item.get("itemNo").toString()), "item");
+				// result =
+				// fileService.deleteImageAll(Integer.parseInt(item.get("itemNo").toString()),
+				// "item");
 			}
 
 		} catch (Exception e) {
