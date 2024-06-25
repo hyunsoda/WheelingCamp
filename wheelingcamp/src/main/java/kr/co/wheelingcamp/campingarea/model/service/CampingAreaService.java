9439bf6ca4a5c9daf2696bd5c75333aaea7d288b
LEECHUNGs <ragnacrimson@naver.com>
2024-06-25 19:54:55 +0900

    즐거운 하루

--- wheelingcamp/src/main/java/kr/co/wheelingcamp/campingarea/model/service/CampingAreaService.java
+++ wheelingcamp/src/main/java/kr/co/wheelingcamp/campingarea/model/service/CampingAreaService.java
@@ -1,5 +1,23 @@
 package kr.co.wheelingcamp.campingarea.model.service;
 
+import java.util.List;
+import java.util.Map;
+
+import kr.co.wheelingcamp.campingarea.model.dto.CampingArea;
+
 public interface CampingAreaService {
+
+	List<CampingArea> selectHompageAll();
+
+	int deleteAll(List<CampingArea> deleteList);
+
+	/**
+	 * 지역별 캠핑장 목록 탐색
+	 * 
+	 * @param locationNo
+	 * @param cp
+	 * @return
+	 */
+	Map<String, Object> selectCampingAreaAll(int locationNo, int cp);
 
 }
