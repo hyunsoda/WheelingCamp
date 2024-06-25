eb43b640db94682a5fa9610497afba9fd627f32c
JUHAHAH <108599647+JUHAHAH@users.noreply.github.com>
2024-06-25 15:37:21 +0900

    Update SecurityConfig.java

--- wheelingcamp/src/main/java/kr/co/wheelingcamp/common/config/SecurityConfig.java
+++ wheelingcamp/src/main/java/kr/co/wheelingcamp/common/config/SecurityConfig.java
@@ -1,15 +1,15 @@
 package kr.co.wheelingcamp.common.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
 
 	@Bean
@@ -21,8 +21,8 @@ public class SecurityConfig {
 	CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration configuration = new CorsConfiguration();
 
-		configuration.addAllowedOrigin("*");
-		configuration.addAllowedHeader("*");
+		configuration.addAllowedOriginPattern("*");
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
 		configuration.addAllowedMethod("*");
 		configuration.setAllowCredentials(true);
 
@@ -30,4 +30,5 @@ public class SecurityConfig {
 		source.registerCorsConfiguration("/**", configuration);
 		return source;
 	}
+
 }
