656572fea6a711341261cfb912168925e461a579
JUHAHAH <108599647+JUHAHAH@users.noreply.github.com>
2024-06-25 15:43:09 +0900

    asd

--- wheelingcamp/src/main/java/kr/co/wheelingcamp/common/config/SecurityConfig.java
+++ wheelingcamp/src/main/java/kr/co/wheelingcamp/common/config/SecurityConfig.java
@@ -4,12 +4,17 @@ import java.util.Arrays;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
 	@Bean
@@ -31,4 +36,10 @@ public class SecurityConfig {
 		return source;
 	}
 
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.cors(cors -> cors.configurationSource(corsConfigurationSource())).csrf(AbstractHttpConfigurer::disable)
+				.authorizeHttpRequests(request -> request.anyRequest().permitAll());
+		return http.build();
+	}
 }
