259181b4f8790854fe052ed93d112d309c26850c
LEECHUNGs <ragnacrimson@naver.com>
2024-06-26 20:36:22 +0900

    찐막?

--- wheelingcamp/src/main/java/kr/co/wheelingcamp/interest/model/dto/Interest.java
+++ wheelingcamp/src/main/java/kr/co/wheelingcamp/interest/model/dto/Interest.java
@@ -1,6 +1,5 @@
 package kr.co.wheelingcamp.interest.model.dto;
 
-import kr.co.wheelingcamp.cart.model.dto.Cart;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,19 +14,19 @@ import lombok.ToString;
 @AllArgsConstructor
 @Builder
 public class Interest {
-	
-	private int cartNo;		// 장바구니 번호
-	private int itemNo;		// 상품 번호
-	private int memberNo;	// 회원 번호
-	private int saleType;	// 판매유형 (1 - 대여, 2 - 구매)
-	
+
+	private int cartNo; // 장바구니 번호
+	private int itemNo; // 상품 번호
+	private int memberNo; // 회원 번호
+	private int saleType; // 판매유형 (1 - 대여, 2 - 구매)
+
 	private int categoryCode; // 카테고리 코드 1- 차 2- 장비 3- 패키지
-	
-	private String itemName;	// 상품 이름
-	private int price;		// 상품 가격
-	private int sellPrice;	// 구매 가격
-	private String itemImg;		// 상품 이미지
-	
-	private int packageNo;	// 패키지 번호
-	
+
+	private String itemName; // 상품 이름
+	private int price; // 상품 가격
+	private int sellPrice; // 구매 가격
+	private String thumbnail; // 상품 이미지
+
+	private int packageNo; // 패키지 번호
+
 }
